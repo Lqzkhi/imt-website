04102ab66efe88806affbca3c20da2a8c80b9ec4
Lqzkhi <bljia2020@gmail.com>
2026-04-16 22:19:53 -0400

    Configure Astro for Vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  site: 'https://Lqzkhi.github.io',
-  base: '/imt-website',
+  output: 'server',
+  adapter: vercel(),
 });
