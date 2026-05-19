874d251b4148eebdf702293455b9a75e4d99d520
Lqzkhi <bljia2020@gmail.com>
2026-05-19 00:32:45 -0400

    Add sitemap and robots.txt for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
+  site: 'https://integratedmathtournament.org',
+  integrations: [tailwind(), sitemap()]
 });
